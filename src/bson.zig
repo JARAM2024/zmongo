79ff61b887d8612ac6abab7b8dea1336c5f77956
sugarme <thangtran@hotmail.com>
2024-03-10 01:03:34 +1100

    bson document append

--- src/bson.zig
+++ src/bson.zig
@@ -38,8 +38,8 @@ pub const BsonError = struct {
 
     var bson_error: c.bson_error_t = undefined;
 
-    pub fn init() Self {
-        return Self{};
+    pub fn init() BsonError {
+        return BsonError{};
     }
 
     pub fn ptr(_: Self) *c.bson_error_t {
@@ -67,34 +67,53 @@ pub const Json = struct {
     pub fn free(self: *const Self) void {
         c.bson_free(self.value);
     }
+
+    pub fn string(self: *const Self) []const u8 {
+        return std.mem.sliceTo(self.value, 0);
+    }
 };
 
 /// Bson is wrapper of c pointer to `bson_t` structure.
 pub const Bson = struct {
-    value: c.bson_t, // pointer to C BSON struct
+    bson: c.bson_t, // pointer to C BSON struct
 
     const Self = @This();
 
-    fn get(self: *Self) *c.bson_t {
-        return &self.value;
+    fn ptr(self: *Self) *c.bson_t {
+        return &self.bson;
     }
 
-    fn getConst(self: *const Self) *const c.bson_t {
-        return &self.value;
+    fn ptrConst(self: *const Self) *const c.bson_t {
+        return &self.bson;
     }
 
-    pub fn new() !Self {
-        if (c.bson_new()) |*ptr| {
-            return Self{
-                .ptr = ptr,
+    pub fn new() Self {
+        const b = c.bson_new();
+        return Bson{
+            .bson = b.*,
+        };
+    }
+
+    /// The newFromJson() function allocates and initializes a new bson_t by parsing the JSON found in data.
+    /// Only a single JSON object may exist in data or an error will be set and NULL returned.
+    ///
+    /// A newly allocated bson_t if successful, otherwise NULL and error is set.
+    pub fn newFromJson(data: [:0]const u8) !Bson {
+        var err = BsonError.init();
+
+        const bs = c.bson_new_from_json(data, -1, err.ptr());
+        if (bs != null) {
+            return Bson{
+                .bson = bs.*,
             };
+        } else {
+            std.debug.print("newFromJson() failed: {s}\n", .{err.message()});
+            return Error.BsonError;
         }
-
-        return Error.BsonError;
     }
 
     pub fn init(self: *Self) void {
-        return c.bson_init(self.get());
+        return c.bson_init(self.ptr());
     }
 
     pub fn deinit(self: *Self) void {
@@ -102,16 +121,16 @@ pub const Bson = struct {
     }
 
     pub fn destroy(self: *Self) void {
-        c.bson_destroy(self.get());
+        c.bson_destroy(self.ptr());
     }
 
     pub fn appendInt32(self: *Self, key: [:0]const u8, value: i32) !void {
-        if (!c.bson_append_int32(self.get(), key, -1, value))
+        if (!c.bson_append_int32(self.ptr(), key, -1, value))
             return Error.AppendError;
     }
 
     pub fn appendUtf8(self: *Self, key: [:0]const u8, value: [:0]const u8) !void {
-        const ok = c.bson_append_utf8(self.get(), key, -1, value, -1);
+        const ok = c.bson_append_utf8(self.ptr(), key, -1, value, -1);
         if (!ok) {
             return Error.AppendError;
         }
@@ -125,12 +144,12 @@ pub const Bson = struct {
     /// calling bson_free() with the result.
     pub fn arrayAsJson(self: *Self) !Json {
         var l: usize = undefined;
-        if (c.bson_array_as_json(self.get(), &l)) |value| {
+        if (c.bson_array_as_json(self.ptr(), &l)) |str| {
             return Json{
-                .value = value,
+                .value = str,
             };
         } else {
-            return Error.BsonError;
+            return Error.JsonError;
         }
     }
 
@@ -141,8 +160,8 @@ pub const Bson = struct {
     /// The bson_append_array() function shall append array to bson using the specified key.
     /// The type of the field will be an array, but it is the responsibility of the caller
     /// to ensure that the keys of array are properly formatted with string keys such as “0”, “1”, “2” and so forth.
-    pub fn appenArray(self: *Self, key: [:0]const u8, array: *Bson) !void {
-        const ok = c.bson_append_array(self.get(), key, -1, array.get());
+    pub fn appenArray(self: *Self, key: [:0]const u8, array: *const Bson) !void {
+        const ok = c.bson_append_array(self.ptr(), key, -1, array.ptrConst());
         if (!ok) {
             return Error.BsonError;
         }
@@ -150,14 +169,14 @@ pub const Bson = struct {
         return;
     }
 
-    /// The bson_append_array_begin() function shall begin appending an array field to bson.
+    /// The appendArrayBegin() function shall begin appending an array field to bson.
     /// This allows for incrementally building a sub-array.
     /// Doing so will generally yield better performance as you will serialize to a single buffer.
     /// When done building the sub-array, the caller MUST call bson_append_array_end().
     ///
     /// For generating array element keys, see bson_uint32_to_string().
     pub fn appendArrayBegin(self: *Self, key: [:0]const u8, child: *Bson) !void {
-        const ok = c.bson_append_array_begin(self.get(), key, -1, child.get());
+        const ok = c.bson_append_array_begin(self.ptr(), key, -1, child.ptr());
         if (!ok) {
             return Error.BsonError;
         }
@@ -165,23 +184,91 @@ pub const Bson = struct {
         return;
     }
 
-    pub fn appendArrayEnd(self: *Self, child: Bson) !void {
-        const ok = c.bson_append_array_end(self.get(), child.get());
+    pub fn appendArrayEnd(self: *Self, child: *Bson) !void {
+        const ok = c.bson_append_array_end(self.ptr(), child.ptr());
         if (!ok) {
-            return Error.BsonError;
+            return Error.AppendError;
         }
 
         return;
     }
 
-    /// appends a new element to bson containing the binary data provided.
+    /// The appendBinary() function shall append a new element to bson containing the binary data provided.
     /// https://mongoc.org/libbson/current/bson_append_binary.html
     pub fn appendBinary(self: *Self, key: [:0]const u8, binary: [:0]u8) !void {
-        const ok = c.bson_append_array_begin(self.get(), key, -1, binary, -1);
+        const ok = c.bson_append_array_begin(self.ptr(), key, -1, binary, -1);
         if (!ok) {
             return Error.BsonError;
         }
 
         return;
+    }
+
+    /// The appendDateTime() function shall append a new element to a bson document containing
+    /// a date and time with no timezone information.
+    /// Value is assumed to be in UTC format of milliseconds since the UNIX epoch. value MAY be negative.
+    pub fn appendDateTime(self: *Self, key: [:0]const u8, value: i64) !void {
+        const ok = c.bson_append_date_time(self.ptr(), key, -1, value);
+        if (!ok) {
+            return Error.AppendError;
+        }
+
+        return;
+    }
+
+    /// The appendDocument() function shall append child to bson using the specified key.
+    ///
+    /// The type of the field will be a document.
+    pub fn appendDocument(self: *Self, key: [:0]const u8, child: *Bson) !void {
+        const ok = c.bson_append_document(self.ptr(), key, -1, child.ptrConst());
+        if (!ok) {
+            return Error.AppendError;
+        }
+
+        return;
+    }
+
+    /// The appendDocumentBegin() function shall begin appending a sub-document to bson.
+    /// Use child to add fields to the sub-document.
+    /// When completed, call appendDocumentEnd() to complete the element.
+    ///
+    /// child MUST be an uninitialized bson_t to avoid leaking memory.
+    pub fn appendDocumentBegin(self: *Self, key: [:0]const u8, child: *Bson) !void {
+        const ok = c.bson_append_document_begin(self.ptr(), key, -1, child.ptr());
+        if (!ok) {
+            return Error.AppendError;
+        }
+
+        return;
+    }
+
+    /// The appendDocumentEnd() function shall complete the appending of a document with appendDocumentBegin().
+    ///
+    /// child is invalid after calling this function.
+    pub fn appendDocumentEnd(self: *Self, child: *Bson) !void {
+        const ok = c.bson_append_document_end(self.ptr(), child.ptr());
+        if (!ok) {
+            return Error.AppendError;
+        }
+
+        return;
+    }
+
+    ///The asCanonicalExtendedJson() encodes bson as a UTF-8 string in the canonical MongoDB Extended JSON format.
+    ///
+    ///The caller is responsible for freeing the resulting UTF-8 encoded string by calling bson_free() with the result.
+    ///
+    /// If non-NULL, length will be set to the length of the result in bytes.
+    ///
+    /// If successful, a newly allocated UTF-8 encoded string and length is set.
+    /// Upon failure, NULL is returned.
+    pub fn asCanonicalExtendedJson(self: *const Self) !Json {
+        if (c.bson_as_canonical_extended_json(self.ptrConst(), null)) |json_str| {
+            return Json{
+                .value = json_str,
+            };
+        }
+
+        return Error.BsonError;
     }
 };
--- src/bson_test.zig
+++ src/bson_test.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const bson = @import("bson.zig");
 
+const datetime = @import("datetime.zig");
+
+const testing = std.testing;
+
 test "append array" {
     var b: bson.Bson = undefined;
     b.init();
@@ -24,5 +28,96 @@ test "bson_as_json" {
 
     const json = try b.arrayAsJson();
     defer json.free();
-    std.debug.print("Json string: {s}\n", .{json.value}); // Json string: [ "Item 1", "Item 2" ]
+    std.debug.print("Json string: {s}\n", .{json.string()}); // Json string: [ "Item 1", "Item 2" ]
+}
+
+test "creating bson document" {
+    var document = bson.Bson.new();
+    // RFC3339 format: https://www.rfc-editor.org/rfc/rfc3339
+    const born = try datetime.DateTime.parseRFC3339("1906-12-09T00:00:00Z");
+    const died = try datetime.DateTime.parseRFC3339("1992-01-01T00:00:00Z");
+
+    // append date-time
+    try document.appendDateTime("born", born.unix(.milliseconds));
+    try document.appendDateTime("died", died.unix(.milliseconds));
+
+    // append child document
+    var child: bson.Bson = undefined;
+    child.init();
+    defer child.deinit();
+    try document.appendDocumentBegin("name", &child);
+    try child.appendUtf8("first", "Grace");
+    try child.appendUtf8("last", "Hopper");
+    try document.appendDocumentEnd(&child);
+
+    // append child array
+    var arr: bson.Bson = undefined;
+    arr.init();
+    defer arr.deinit();
+    try document.appendArrayBegin("degrees", &arr);
+    try arr.appendUtf8("degree", "BA");
+    try arr.appendUtf8("school", "Vassar");
+    try document.appendArrayEnd(&arr);
+
+    const json = try document.asCanonicalExtendedJson();
+    defer json.free();
+
+    std.debug.print("json: \n{s}\n", .{json.string()});
+
+    // {
+    //      "born": {
+    //          "$date": {
+    //              "$numberLong": "-1990137600000"
+    //          }
+    //      },
+    //      "died": {
+    //          "$date": {
+    //              "$numberLong": "694224000000"
+    //          }
+    //      },
+    //      "name": {
+    //          "first": "Grace",
+    //          "last": "Hopper"
+    //      },
+    //      "degrees": [
+    //          "BA",
+    //          "Vassar"
+    //      ]
+    // }
+    //
+    //
+}
+
+test "json bson round trip" {
+    const json_input =
+        \\{
+        \\ "born": {
+        \\   "$date": {
+        \\      "$numberLong": "-1990137600000"
+        \\    }
+        \\  },
+        \\  "died": {
+        \\    "$date": {
+        \\      "$numberLong": "694224000000"
+        \\    }
+        \\  },
+        \\  "name": {
+        \\    "first": "Grace",
+        \\    "last": "Hopper"
+        \\  },
+        \\  "degrees": [
+        \\    "BA",
+        \\    "Vassar"
+        \\  ]
+        \\}
+    ;
+
+    const document = try bson.Bson.newFromJson(json_input);
+
+    const json_output = try document.asCanonicalExtendedJson();
+    defer json_output.free();
+
+    std.debug.print("json: \n{s}\n", .{json_output.string()});
+
+    // try testing.expectEqualStrings(json_input, json_output.string());
 }
